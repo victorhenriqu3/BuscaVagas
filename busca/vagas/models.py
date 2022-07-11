f81908a3e154f781dc21dcbd0931eb891efa3654
victorhenriqu3 <victor.lima@going2.com.br>
2022-07-11 18:44:15 -0400

    Create Model Employee

--- busca/vagas/models.py
+++ busca/vagas/models.py
@@ -1,3 +1,12 @@
+
+import email
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    name = models.CharField(db_column='name', max_length=100, blank=False)
+    description = models.TextField(db_column='description', max_length=1000)
+    dailyRate = models.DecimalField(db_column='daily',decimal_places=2,max_digits=5, blank=False)
+    birthday = models.DateField(db_column='bday', blank=False)
+    cpf = models.CharField(db_column='cpf' ,max_length=11, blank=False)
+    email = models.EmailField(db_column='email', blank=False)

4878933bcb9a24b81ee11fa30ab30b04a96fa5d9
Victor Henrique <victorhenriqu3@outlook.com>
2022-07-13 00:14:19 -0400

    Create Employee List screen

---
+++ busca/vagas/forms.py
@@ -0,0 +1,9 @@
+from dataclasses import field
+from django.forms import ModelForm
+from .models import Employee
+
+
+class EmployeeForm(ModelForm):
+  class Meta:
+    model = Employee
+    fields = '__all__'
--- busca/vagas/views.py
+++ busca/vagas/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from .models import Employee
+
 # Create your views here.
+def employeeList(request):
+  employees = Employee.objects.all()
+  return render(request,"employeeList.html",{'employees':employees})
